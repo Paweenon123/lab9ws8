224053c7fe915d66ba36617e6488a70cd374c241
Paweenon123 <94670892+Paweenon123@users.noreply.github.com>
2022-10-10 20:13:03 +0700

    Add files via upload

---
+++ 8.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+    <form action="sendredict.php" method="post">
+        ๊ Username: <input type="text" name="username"><br><br>
+        รหัสผ่าน: <input type="password" name="password"><br><br>
+        ชื่อ: <input type="text" name="name"><br><br>
+        ที่อยู่: <textarea name="address" rows="3" cols="40"></textarea><br><br>
+        เบอร์โทร: <input type="text" name="mobile" pattern="[0-9]{10}" title="Please try again"><br><br>
+        อีเมล: <input type="text" name="email"><br><br>
+        <input type="submit" value="เพิ่มสมาชิก">
+    </form>
+</body>
+
+</html>
